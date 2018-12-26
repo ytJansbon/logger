96c1c6ebef361bd8711c00ded3900fd15baead4d
ytJansbon <176038687@qq.com>
2018-12-26 10:38:12 +0800

    add singleton interface.

---
+++ example/example_singleton/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"logger"
+	"os/user"
+	"time"
+)
+
+func main() {
+	user, err := user.Current()
+	config := "{" +
+		"\"prefix\":\"mylogger\"," +
+		"\"bufferSize\":1024," +
+		"\"fileDir\":\"%s\"," +
+		"\"fileName\":\"example.log\"," +
+		"\"fileSize\":%d," +
+		"\"fileCount\":5," +
+		"\"outputType\":%d" +
+		"}"
+	config = fmt.Sprintf(config, user.HomeDir+"/log", 1024*1024*1, logger.OutputType_console)
+
+	err = logger.InsLogger().Init(config)
+	if err != nil {
+		fmt.Println("logger new error:", err.Error())
+		return
+	}
+
+	logger.InsLogger().Info("test logger info")
+	time.Sleep(time.Duration(5) * time.Second)
+	fmt.Println("test example_singleton end")
+}
